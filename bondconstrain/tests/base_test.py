e82ab3210a154f1aa572fb9172fe1552b0839067
Ryan S. DeFever <r.s.defever@gmail.com>
2020-11-02 13:30:31 -0500

    Add class for constraining bonds with unit tests

---
+++ bondconstrain/tests/base_test.py
@@ -0,0 +1,60 @@
+import pytest
+import mbuild
+import foyer
+import parmed
+import numpy as np
+
+
+class BaseTest:
+
+    @pytest.fixture
+    def ethane_ua(self):
+        ethane = parmed.Structure()
+        a1 = parmed.topologyobjects.Atom(name="C", atomic_number=6)
+        a2 = parmed.topologyobjects.Atom(name="C", atomic_number=6)
+        ethane.add_atom(a1, resname="RES", resnum=1)
+        ethane.add_atom(a2, resname="RES", resnum=1)
+        bond_type = parmed.topologyobjects.BondType(1.0, 1.0)
+        bond = parmed.topologyobjects.Bond(a1, a2, type=bond_type)
+        ethane.bonds.append(bond)
+        ethane.bond_types.append(bond_type)
+        ethane.coordinates = np.array([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0]])
+        return ethane
+
+    @pytest.fixture
+    def propane_ua(self):
+        propane = parmed.Structure()
+        a1 = parmed.topologyobjects.Atom(name="C", atomic_number=6)
+        a2 = parmed.topologyobjects.Atom(name="C", atomic_number=6)
+        a3 = parmed.topologyobjects.Atom(name="C", atomic_number=6)
+        propane.add_atom(a1, resname="RES", resnum=1)
+        propane.add_atom(a2, resname="RES", resnum=1)
+        propane.add_atom(a3, resname="RES", resnum=1)
+        bond_type = parmed.topologyobjects.BondType(1.5, 1.5)
+        b1 = parmed.topologyobjects.Bond(a1, a2, type=bond_type)
+        b2 = parmed.topologyobjects.Bond(a2, a3, type=bond_type)
+        propane.bonds.append(b1)
+        propane.bonds.append(b2)
+        propane.bond_types.append(bond_type)
+        propane.coordinates = np.array(
+            [
+                [0.0, 0.1, 0.2],
+                [1.0, 0.3, 0.1],
+                [2.0, 0.4, 0.2]
+            ]
+        )
+        return propane
+
+    @pytest.fixture
+    def dimehtylether_oplsaa(self):
+        ff = foyer.forcefields.load_OPLSAA()
+        dme = mbuild.load("COC", smiles=True)
+        dme = ff.apply(dme)
+        return dme
+
+    @pytest.fixture
+    def benzene_oplsaa(self):
+        ff = foyer.forcefields.load_OPLSAA()
+        benzene = mbuild.load("c1ccccc1", smiles=True)
+        benzene = ff.apply(benzene)
+        return benzene
---
+++ bondconstrain/tests/test_constrainmol.py
@@ -0,0 +1,122 @@
+import pytest
+import parmed
+import pyomo.environ as pyo
+import numpy as np
+
+from copy import deepcopy
+from bondconstrain.tests.base_test import BaseTest
+from bondconstrain.constrainmol import ConstrainMol
+
+
+class TestConstrainMol(BaseTest):
+    def test_invalid_init(self):
+        with pytest.raises(TypeError):
+            ConstrainMol("structure")
+
+    def test_no_bonds(self):
+        with pytest.raises(ValueError):
+            ConstrainMol(parmed.Structure())
+
+    def test_copy_coordinates(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        assert np.allclose(constrain_mol.structure.coordinates, ethane_ua.coordinates)
+
+    def test_copy_bonds(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        assert len(constrain_mol.structure.bonds) == len(ethane_ua.bonds)
+
+    def test_create_model(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        assert isinstance(constrain_mol.model, pyo.ConcreteModel)
+
+    def test_model_xyz(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        assert np.allclose(constrain_mol.model.x_start[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x_start[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y_start[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y_start[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z_start[1].value, ethane_ua.coordinates[1, 2])
+        assert np.allclose(constrain_mol.model.z_start[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.x[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.z[1].value, ethane_ua.coordinates[1, 2])
+
+    def test_model_constraints(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        assert np.allclose(constrain_mol.model.bond_lengths[(0, 1)], ethane_ua.bonds[0].type.req)
+
+    def test_solved_model(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        constrain_mol.solve()
+        assert np.allclose(constrain_mol.model.x_start[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x_start[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y_start[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y_start[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z_start[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.z_start[1].value, ethane_ua.coordinates[1, 2])
+        assert np.allclose(constrain_mol.model.x[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.z[1].value, ethane_ua.coordinates[1, 2])
+
+    def test_invalid_update(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        with pytest.raises(TypeError):
+            constrain_mol.update_xyz("string")
+        with pytest.raises(ValueError):
+            constrain_mol.update_xyz([[1.0, 1.0, 1.0]])
+
+    def test_update_xyz(self, ethane_ua):
+        constrain_mol = ConstrainMol(ethane_ua)
+        ethane_ua.coordinates[0, 0] = -1.0
+        ethane_ua.coordinates[0, 1] = -0.3
+        ethane_ua.coordinates[0, 2] = 0.2
+        constrain_mol.update_xyz(ethane_ua.coordinates)
+        assert np.allclose(constrain_mol.structure.coordinates, ethane_ua.coordinates)
+        assert np.allclose(constrain_mol.model.x_start[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x_start[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y_start[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y_start[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z_start[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.z_start[1].value, ethane_ua.coordinates[1, 2])
+        assert np.allclose(constrain_mol.model.x[0].value, ethane_ua.coordinates[0, 0])
+        assert np.allclose(constrain_mol.model.x[1].value, ethane_ua.coordinates[1, 0])
+        assert np.allclose(constrain_mol.model.y[0].value, ethane_ua.coordinates[0, 1])
+        assert np.allclose(constrain_mol.model.y[1].value, ethane_ua.coordinates[1, 1])
+        assert np.allclose(constrain_mol.model.z[0].value, ethane_ua.coordinates[0, 2])
+        assert np.allclose(constrain_mol.model.z[1].value, ethane_ua.coordinates[1, 2])
+
+    def test_resolve_model(self, propane_ua):
+        constrain_mol = ConstrainMol(propane_ua)
+        constrain_mol.solve()
+        propane_solved = deepcopy(constrain_mol.structure)
+        constrain_mol.update_xyz(propane_ua.coordinates)
+        constrain_mol.solve()
+        assert np.allclose(propane_solved.coordinates, constrain_mol.structure.coordinates)
+
+    def test_dimethylether(self, dimehtylether_oplsaa):
+        constrain_mol = ConstrainMol(dimehtylether_oplsaa)
+        constrain_mol.solve()
+        optimized = constrain_mol.structure
+        xyz = optimized.coordinates
+        for bond in optimized.bonds:
+            idx1 = bond.atom1.idx
+            idx2 = bond.atom2.idx
+            dist = np.sqrt(np.sum((xyz[idx2] - xyz[idx1])**2))
+            assert np.allclose(dist, bond.type.req)
+
+    def test_benzene(self, benzene_oplsaa):
+        constrain_mol = ConstrainMol(benzene_oplsaa)
+        constrain_mol.solve()
+        optimized = constrain_mol.structure
+        xyz = optimized.coordinates
+        for bond in optimized.bonds:
+            idx1 = bond.atom1.idx
+            idx2 = bond.atom2.idx
+            dist = np.sqrt(np.sum((xyz[idx2] - xyz[idx1])**2))
+            assert np.allclose(dist, bond.type.req)
